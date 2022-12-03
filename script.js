26e3de3c3e4ec20f95c0dad376912f4a7125db97
mar <rojanomariano@gmail.com>
2022-12-02 21:21:09 -0800

    more starter code

---
+++ script.js
@@ -0,0 +1,25 @@
+var questions = [{
+    question: "Which is a JavaScript Data Type?",
+    choiceA: "Number",
+    choiceB: "String",
+    choiceC: "Boolean",
+    choiceD: "Object",
+    correct choice: "All of the above",
+},
+{
+    question: "Which of the following is a type of Pop up box available in JavaScript?",
+    choiceA: "loop",
+    choiceB: "Declaration",
+    choiceC: "Scope",
+    correct choice: "Alert",
+},
+{
+    question: "W",
+    choiceA: "loop",
+    choiceB: "Declaration",
+    choiceC: "Scope",
+    correct choice: "Alert",
+},
+
+
+]
