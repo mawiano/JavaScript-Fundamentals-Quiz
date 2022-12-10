2d649059a5e6c808096894b68860c5ad59ae2db2
mar <rojanomariano@gmail.com>
2022-12-09 20:12:41 -0800

    added timer, updates questions, added JS to start timer

--- script.js
+++ script.js
@@ -1,31 +1,71 @@
-var questionEl = document.getElementById("question");
+var questionEl = document.querySelector("#question");
 var currentIndex = 0
-var startButton = document.querySelector("#Begin");
+var startButton = document.querySelector("#Begin-button");
 var questionsContainer = document.querySelector("#questions-container");
+var startPage = document.querySelector("#start-page");
+var timer = document.querySelector(".timer")
+var timeRemaining= 60;
+var answerEl= document.querySelector(".answers")
 
-
-
-var questions = [
+var quizQA = [
     {
-    question: "1Which is a JavaScript Data Type?",
-    choices: ["Number", "String", "Boolean", "All of the Above"],
+    question: "Which is a JavaScript Data Type?",
+    options: ["Number", "String", "Boolean", "All of the Above"],
     correctChoice: "All of the above"
 },
 {
-    question: "2Which is a JavaScript Data Type?",
-    choices: ["Number", "String", "Boolean", "All of the Above"],
-    correctChoice: "All of the above"
+    question: "When was JavaScript created?",
+    options: ["2001", "1995", "1987", "2005"],
+    correctChoice: "1995"
 },
 {
-    question: "3Which is a JavaScript Data Type?",
-    choices: ["Number", "String", "Boolean", "All of the Above"],
-    correctChoice: "All of the above"
+    question: "What was the original name for JavaScript?",
+    options: ["Mocha", "JavaChip", "VanillaBean", "LavaScript"],
+    correctChoice: "Mocha"
+},
+{
+    question: "How do you denote an array in JavaScript?",
+    options: ["()", "{}", "//", "[]"],
+    correctChoice: "[]"
 },
 ]
 function beginQuiz(){
-    // startTimer();
-questionsContainer.setAttribute("class", "hidden");
+    startTimer();
+startPage.setAttribute("class", "hidden");
 }
+
+function startTimer(){
+    clockInterval= setInterval(function(){
+        timeRemaining--; 
+        timer.textContent= "time remaining: " + timeRemaining;
+        if (timeRemaining<= 0){
+            clearInterval(clockInterval);
+            timeRemaining= 0; 
+            timer.textContent="";
+            endQuiz();
+            questionsContainer.innerHTML="";
+
+        }
+    }, 1000);
+}
+
+
+function displayQuestion(questionIndex){
+    questionEl.textContent= quizQA[questionIndex].question;
+questionsContainer.append(questionEl);
+answerEl.textContent= "";
+for (var i=0; i<quizQA[questionIndex].options.length; i++){
+    
+}
+}
+
+
 
 startButton.addEventListener("click", beginQuiz);
 
+
+
+// Click start button, Run timer, show questions and choices
+// Click on answer choice, go to next question.
+// End of quiz, or time up, go to end page.
+// Enter initials, save to local storage along with score, print to page on finals scores list. 
